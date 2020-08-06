691e8d2aaea53f622ef3a150f27694b4bf2d1912
MrEverything <nampp1204@gmail.com>
2020-08-06 13:47:18 +0700

    add file

---
+++ nearest-neighbor/cmake-build-debug/CMakeFiles/nearest_neighbor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nearest_neighbor.dir/main.cpp.obj"
+  "nearest_neighbor.exe"
+  "nearest_neighbor.exe.manifest"
+  "nearest_neighbor.lib"
+  "nearest_neighbor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nearest_neighbor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main/java/com/phuongnam/controller/DesignTacoController.java
@@ -0,0 +1,63 @@
+package com.phuongnam.controller;
+
+
+import com.phuongnam.model.Ingredient;
+import com.phuongnam.model.Ingredient.Type;
+import com.phuongnam.model.Taco;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        List<Ingredient> ingrList = new ArrayList<Ingredient>();
+        for (Ingredient ingredient : ingredients) {
+            if (ingredient.getType().equals(type))
+                ingrList.add(ingredient);
+        }
+        return ingrList;
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco taco, Errors errors) {
+        if (errors.hasErrors()) {
+            return "home";
+        }
+        // Save the taco design...
+        // We'll do this later
+//        log.info("Processing design: " + taco);
+        return "redirect:/orders/current";
+    }
+}
+
